475bbc13c373a99e77a6ee473d00f40e080564a1
oreoluwade <oreoluwade@gmail.com>
2019-11-22 16:58:36 +0000

    Updates

---
+++ precache-manifest.3be4fdc2aa7ae80084e3a7ab13469ce0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eed868139c02e9c6ddcf60521e9198e",
+    "url": "/durator/index.html"
+  },
+  {
+    "revision": "dbbaff5d11a7aae608b9",
+    "url": "/durator/static/css/main.12215fda.chunk.css"
+  },
+  {
+    "revision": "9d03543fbce9b67da81c",
+    "url": "/durator/static/js/2.bb988f05.chunk.js"
+  },
+  {
+    "revision": "dbbaff5d11a7aae608b9",
+    "url": "/durator/static/js/main.db4ebd6c.chunk.js"
+  },
+  {
+    "revision": "68fbe1e08e35ccf1b9e0",
+    "url": "/durator/static/js/runtime-main.7ce13e6c.js"
+  },
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/durator/static/media/digital-7-mono.58045dab.ttf"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/durator/static/media/digital-7.1e670d88.ttf"
+  }
+]);
