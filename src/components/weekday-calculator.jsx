278c9dffd0358b8d5fa0743a560aa8f4982c94fd
oreoluwade <oreoluwade@gmail.com>
2019-11-25 12:10:52 +0000

    Complete weekday calculator feature

--- src/components/weekday-calculator.jsx
+++ src/components/weekday-calculator.jsx
@@ -1,58 +1,130 @@
-import React, { useState } from 'react';
-import { MONTH_OPTIONS } from '../util/select-options';
+import React, { useState, useEffect } from 'react';
+import { MONTH_OPTIONS, getDayOptions } from '../util/select-options';
+import { checkYearValidity, getDayString } from '../util/helpers';
 
 const WeekdayCalculator = () => {
-  const [date, setDate] = useState({ day: '', month: '', year: '' });
+    const [date, setDate] = useState({ day: '', month: '', year: '' });
+    const [inputIsValid, setInputIsValid] = useState(false);
+    const [result, setResult] = useState(null);
 
-  const handleInputChange = e => {
-    e.persist();
-    const newDate = Object.assign({}, date, {
-      [e.target.name]: e.target.value
-    });
-    setDate(newDate);
-    console.log('Date', date);
-  };
+    useEffect(() => {
+        const inputItemsAreValid = Object.values(date).every(item =>
+            Boolean(item)
+        );
 
-  return (
-    <div className="wd">
-      <p>Enter your birth date below</p>
-      <form className="wd-date-group">
-        <div className="wd-field">
-          <select className="wd-month-selector">
-            {MONTH_OPTIONS.map((mo, index) => (
-              <option value="mo" key={index} className="wd-option">
-                {mo}
-              </option>
-            ))}
-          </select>
-          <label htmlFor="month">Month</label>
+        setInputIsValid(inputItemsAreValid);
+    }, [date]);
+
+    const handleInputChange = e => {
+        e.persist();
+        const numOfCharsIsValid = e.target.value.length <= 4;
+
+        if (
+            e.target.name === 'year' &&
+            !checkYearValidity(e.target.value) &&
+            numOfCharsIsValid
+        ) {
+            return setDate({
+                ...date,
+                year: e.target.value.slice(0, e.target.value.length - 1)
+            });
+        } else {
+            if (numOfCharsIsValid) {
+                setDate({ ...date, [e.target.name]: e.target.value });
+                console.log('Date', date);
+            }
+        }
+    };
+
+    const getDayOfWeek = () => {
+        const constructedDate = new Date(
+            `${date.year}-${date.month}-${date.day}`
+        );
+
+        setResult(getDayString(new Date(constructedDate)));
+    };
+
+    return (
+        <div className="wd">
+            <p>Enter your birth date below</p>
+            <form className="wd-date-group">
+                <div className="wd-field">
+                    <input
+                        className="wd-input"
+                        type="text"
+                        placeholder="YYYY"
+                        value={date.year}
+                        name="year"
+                        onChange={handleInputChange}
+                    />
+                    <label htmlFor="year">Year</label>
+                </div>
+                <div className="wd-field">
+                    <select
+                        className={
+                            !date.month
+                                ? 'wd-selector wd-selector__disabled'
+                                : 'wd-selector'
+                        }
+                        onChange={handleInputChange}
+                        name="month"
+                    >
+                        <option value="" className="wd-option">
+                            Select a month
+                        </option>
+                        {MONTH_OPTIONS.map(mo => (
+                            <option
+                                value={mo.num}
+                                key={mo.str}
+                                className="wd-option"
+                            >
+                                {mo.str}
+                            </option>
+                        ))}
+                    </select>
+                    <label htmlFor="month">Month</label>
+                </div>
+                <div className="wd-field">
+                    <select
+                        className={
+                            !date.day
+                                ? 'wd-selector wd-selector__disabled'
+                                : 'wd-selector'
+                        }
+                        onChange={handleInputChange}
+                        name="day"
+                        disabled={!date.month}
+                    >
+                        <option value="" className="wd-option">
+                            Select a day
+                        </option>
+                        {getDayOptions(date.month, date.year).map(day => (
+                            <option value={day} key={day} className="wd-option">
+                                {day}
+                            </option>
+                        ))}
+                    </select>
+                    <label htmlFor="day">Day</label>
+                </div>
+
+                <button
+                    type="button"
+                    className="wd-submit"
+                    onClick={getDayOfWeek}
+                    disabled={!inputIsValid}
+                >
+                    SUBMIT
+                </button>
+            </form>
+
+            {result && (
+                <h1 className="wd-result">
+                    You were born on a{' '}
+                    <span className="wd-result_calculated">Friday</span>
+                </h1>
+            )}
         </div>
-        <div className="wd-field">
-          <input
-            className="wd-input"
-            type="text"
-            value={date.day}
-            name="day"
-            onChange={handleInputChange}
-          />
-          <label htmlFor="day">Day</label>
-        </div>
-        <div className="wd-field">
-          <input
-            className="wd-input"
-            type="text"
-            value={date.year}
-            name="year"
-            onChange={handleInputChange}
-          />
-          <label htmlFor="year">Year</label>
-        </div>
-        <button type="button" className="wd-submit">
-          SUBMIT
-        </button>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default WeekdayCalculator;
--- src/util/select-options.js
+++ src/util/select-options.js
@@ -1,14 +1,51 @@
+const createFiniteArray = (start, stop, step) => {
+    const newArray = [];
+    for (let i = start; i <= stop; i += step) {
+        newArray.push(i);
+    }
+    return newArray;
+};
+
 export const MONTH_OPTIONS = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December'
+    { str: 'January', num: '01' },
+    { str: 'February', num: '02' },
+    { str: 'March', num: '03' },
+    { str: 'April', num: '04' },
+    { str: 'May', num: '05' },
+    { str: 'June', num: '06' },
+    { str: 'July', num: '07' },
+    { str: 'August', num: '08' },
+    { str: 'September', num: '09' },
+    { str: 'October', num: '10' },
+    { str: 'November', num: '11' },
+    { str: 'December', num: '12' }
 ];
+
+const thirtyOneDayMonths = [
+    'January',
+    'March',
+    'May',
+    'July',
+    'August',
+    'October',
+    'December'
+];
+
+const thirtyDayMonths = ['April', 'June', 'September', 'November'];
+
+export const getDayOptions = (month, year) => {
+    let options;
+    if (thirtyOneDayMonths.includes(month)) {
+        options = createFiniteArray(1, 31, 1);
+    } else if (thirtyDayMonths.includes(month)) {
+        options = createFiniteArray(1, 30, 1);
+    } else {
+        if (year % 4 === 0) {
+            options = createFiniteArray(1, 29, 1);
+        } else {
+            options = createFiniteArray(1, 28, 1);
+        }
+    }
+
+    return options;
+};
